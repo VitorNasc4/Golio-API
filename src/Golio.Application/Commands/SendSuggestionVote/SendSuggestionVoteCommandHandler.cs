9392d2ceb64bb9350ae322a60ae1730eea3e6070
vitor <vitornascimento@321@hotmail.com>
2024-05-28 15:06:03 -0300

    chore: sending email on vote message

--- src/Golio.Application/Commands/SendSuggestionVote/SendSuggestionVoteCommandHandler.cs
+++ src/Golio.Application/Commands/SendSuggestionVote/SendSuggestionVoteCommandHandler.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Golio.Core.DTOs;
 using Golio.Core.Repositories;
 using Golio.Core.Services;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Golio.Application.Commands.CreateProduct.SendSuggestion
 {
@@ -12,13 +14,17 @@ namespace Golio.Application.Commands.CreateProduct.SendSuggestion
     {
         private readonly ISuggestionRepository _suggestionRepository;
         private readonly IMessageBusService _messageBusService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public SendSuggestionVoteCommandHandler(
             ISuggestionRepository suggestionRepository,
-            IMessageBusService messageBusService)
+            IMessageBusService messageBusService,
+            IHttpContextAccessor httpContextAccessor
+            )
         {
             _suggestionRepository = suggestionRepository;
             _messageBusService = messageBusService;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<Unit> Handle(SendSuggestionVoteCommand request, CancellationToken cancellationToken)
         {
@@ -29,11 +35,17 @@ namespace Golio.Application.Commands.CreateProduct.SendSuggestion
                 return Unit.Value;
             }
 
-
+            var userEmail = _httpContextAccessor.HttpContext?.User.Identities.FirstOrDefault().Claims.Where(c => c.Type == "userName").FirstOrDefault().Value;
+            if (string.IsNullOrEmpty(userEmail))
+            {
+                Console.WriteLine("Error getting author's email");
+                return Unit.Value;
+            }
             var suggestionVoteMessage = new SuggestionVoteMessage()
             {
                 SuggestionId = suggestion.Id,
-                IsValid = request.IsValid
+                IsValid = request.IsValid,
+                EmailAutor = userEmail,
             };
 
             await _messageBusService.SendMessageQueueAsync(suggestionVoteMessage);
--- src/Golio.Core/DTOs/SuggestionVoteMessage.cs
+++ src/Golio.Core/DTOs/SuggestionVoteMessage.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Golio.Core.DTOs
 {
     public class SuggestionVoteMessage
     {
         public int SuggestionId { get; set; }
         public bool IsValid { get; set; }
+        public string EmailAutor { get; set; }
     }
 }
